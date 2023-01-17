b7ed944bb37516575b4e86aa21cabaa9ca92468b
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-01-17 17:42:28 -0400

    Muita coisa muita

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,51 @@
 class UserController {
-  constructor(formId, tableId) {
-    this.formEl = document.getElementById(formId);
+  constructor(formIdCreate, formIdUpdate, tableId) {
+    this.formEl = document.getElementById(formIdCreate);
+    this.formUpdateEl = document.getElementById(formIdUpdate);
     this.tableEl = document.getElementById(tableId);
 
     this.onSubmit();
+    this.onEdit();
+  }
+
+  onEdit() {
+    document.querySelector("#box-user-update .btn-cancel").addEventListener("click", (e) => {
+      this.showPanelCreate();
+    });
+
+    this.formUpdateEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let btn = this.formUpdateEl.querySelector("[type=submit]");
+
+      btn.disabled = true;
+
+      let values = this.getValues(this.formUpdateEl);
+
+      let index = this.formUpdateEl.dataset.trIndex;
+
+      let tr = this.tableEl.rows[index];
+
+      tr.dataset.user = JSON.stringify(values);
+
+      tr.innerHTML = `
+        <td>
+            <img src="${values.photo}" alt="User Image" class="img-circle img-sm" />
+                </td>
+                    <td>${values.name}</td>
+                    <td>${values.email}</td>
+                    <td>${values.admin ? "Sim" : "Não"}</td>
+                    <td>${Utils.dateFormat(values.register)}</td>
+                <td>
+            <button type="button" class="btn btn-primary btn-edit btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+        </td>     
+      `;
+
+      this.addEventsTr(tr);
+
+      this.updateCount();
+    });
   }
 
   onSubmit() {
@@ -14,7 +56,7 @@ class UserController {
 
       btn.disabled = true;
 
-      let values = this.getValues();
+      let values = this.getValues(this.formEl);
 
       if (!values) {
         return false;
@@ -64,12 +106,12 @@ class UserController {
     });
   }
 
-  getValues() {
+  getValues(formEl) {
     let user = {};
 
     let isValid = true;
 
-    [...this.formEl.elements].forEach((field, index) => {
+    [...formEl.elements].forEach((field, index) => {
       if (["name", "email", "password"].indexOf(field.name) > -1 && !field.value) {
         field.parentElement.classList.add("has-error");
         isValid = false;
@@ -118,14 +160,56 @@ class UserController {
                     <td>${dataUser.admin ? "Sim" : "Não"}</td>
                     <td>${Utils.dateFormat(dataUser.register)}</td>
                 <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-primary btn-edit btn-flat">Editar</button>
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>     
-  `;
+    `;
+
+    this.addEventsTr(tr);
 
     this.tableEl.appendChild(tr);
 
     this.updateCount();
+  }
+
+  addEventsTr(tr) {
+    tr.querySelector(".btn-edit").addEventListener("click", (e) => {
+      let json = JSON.parse(tr.dataset.user);
+      let form = document.querySelector("#form-user-update");
+
+      form.dataset.trIndex = tr.sectionRowIndex;
+
+      for (let name in json) {
+        let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+        if (field) {
+          switch (field.type) {
+            case "file":
+              continue;
+            case "radio":
+              field = form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
+              field.checked = true;
+              break;
+            case "checkbox":
+              field.checked = json[name];
+              break;
+
+            default:
+              field.value = json[name];
+          }
+        }
+      }
+
+      this.showPanelUpdate();
+    });
+  }
+
+  showPanelCreate() {
+    document.querySelector("#box-user-create").style.display = "block";
+    document.querySelector("#box-user-update").style.display = "none";
+  }
+  showPanelUpdate() {
+    document.querySelector("#box-user-create").style.display = "none";
+    document.querySelector("#box-user-update").style.display = "block";
   }
 
   updateCount() {
@@ -139,8 +223,6 @@ class UserController {
 
       if (user._admin) numberAdmin++;
     });
-
-    console.log(numberAdmin);
 
     document.getElementById("number-users").innerHTML = numberUsers;
     document.getElementById("number-users-admin").innerHTML = numberAdmin;
