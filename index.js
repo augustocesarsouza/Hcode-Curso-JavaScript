70378fa2f46a273d74b5601ff0060cce733f0655
augustocesarsouza <augustocesarsantana90@gmail.com>
2023-01-17 07:46:37 -0400

    Criando class e fazendo um new

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ const fields = document.querySelectorAll("#form-user-create [name]");
 const user = {};
 
 function addLine(dataUser) {
-  let tr = document.createElement("tr");
+  console.log(dataUser);
 
-  tr.innerHTML = `
-    <tr>
-        <td>
-            <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm" />
-                </td>
-                    <td>${dataUser.name}</td>
-                    <td>${dataUser.email}</td>
-                    <td>${dataUser.admin}</td>
-                    <td>${dataUser.birth}</td>
-                <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-    </tr>       
-  `;
-
-  document.getElementById("table-users").appendChild(tr);
-
+  document.getElementById("table-users").innerHTML = `
+  <tr>
+      <td>
+          <img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm" />
+              </td>
+                  <td>${dataUser.name}</td>
+                  <td>${dataUser.email}</td>
+                  <td>${dataUser.admin}</td>
+                  <td>${dataUser.birth}</td>
+              <td>
+          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+      </td>
+  </tr>       
+`;
   console.log(dataUser);
 }
 
@@ -35,8 +32,19 @@ document.getElementById("form-user-create").addEventListener("submit", (e) => {
       }
     } else {
       user[field.name] = field.value;
-      console.log("ascascas");
     }
   });
-  addLine(user);
+
+  const objectUser = new User(
+    user.name,
+    user.gender,
+    user.birth,
+    user.country,
+    user.email,
+    user.password,
+    user.photo,
+    user.admin,
+  );
+
+  addLine(objectUser);
 });
